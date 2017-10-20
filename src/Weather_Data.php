6ddb15ce840165eacf6b4641654c8a0eab88daa4
Clemens Basler <clemens-basler@t-online.de>
2017-10-20 12:26:41 +0200

    init

---
+++ src/Weather_Data.php
@@ -0,0 +1,33 @@
+<?php
+// src/User.php
+/**
+ * @Entity @Table(name="Weather_Data")
+ */
+class Weather_Data
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="integer")
+     * @var string
+     */
+    protected $Sonic-WS10sec;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
